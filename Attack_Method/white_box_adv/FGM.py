0a0dbc82080d564154297af0f53a46cc77593c87
tdlili <108248420+tdlili@users.noreply.github.com>
2022-08-03 10:37:45 +0800

    Update FGM.py

--- Attack_Method/white_box_adv/FGM.py
+++ Attack_Method/white_box_adv/FGM.py
@@ -1,7 +1,5 @@
 import torch
 import numpy as np
-from PIL import Image
-from tqdm import tqdm
 
 from cleverhans.torch.attacks.fast_gradient_method import fast_gradient_method
 
@@ -11,38 +9,40 @@ sefi_component = SEFIComponent()
 
 @sefi_component.attacker_class(attack_name="FGM", perturbation_budget_var_name="epsilon")
 class FGM():
-    def __init__(self, model, T=50, epsilon=0.2, pixel_min=-3, pixel_max=3, alpha=6 / 25, attacktype='untargeted',
-                 tlabel=-1):
+    def __init__(self, model, clip_min=-3, clip_max=3, sanity_checks=False, epsilon=0.2, attacktype='untargeted',tlabel=-1):
         self.model = model  # 待攻击的白盒模型
-        self.T = T  # 迭代攻击轮数
+        self.clip_min = clip_min  # 对抗性示例组件的最小浮点值
+        self.clip_max = clip_max  # 对抗性示例组件的最大浮点值
+        self.sanity_checks = sanity_checks  # 如果为True，包含断言；关闭以使用较少的运行时/内存，或用于故意传递奇怪输入的单元测试
         self.epsilon = epsilon  # 以无穷范数作为约束，设置最大值
-        self.pixel_min = pixel_min  # 像素值的下限
-        self.pixel_max = pixel_max  # 像素值的上限（这与当前图片范围有一定关系，建议0-255，因为对于无穷约束来将不会因为clip原因有一定损失）
-        self.alpha = alpha  # 每一轮迭代攻击的步长
         self.attacktype = attacktype  # 攻击类型：靶向 or 非靶向
         self.tlabel = tlabel
-        self.label = -1
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
-    # 将图片进行clip
-    def clip_value(self, x, ori_x):
-        x = torch.clamp((x - ori_x), -self.epsilon, self.epsilon) + ori_x
-        x = torch.clamp(x, self.pixel_min, self.pixel_max)
-        return x.data
-
-    def get_loss(self):
-        return torch.nn.CrossEntropyLoss()
 
     @sefi_component.attack(name="FGM", is_inclass=True, support_model=["vision_transformer"])
     def attack(self, img, ori_label):
-        batch_size = 16
-        img = torch.from_numpy(img).to(self.device).float()
-        #tlabel_tensor = torch.Tensor([float(self.tlabel)])
-        y = torch.tensor([5]*batch_size,device=self.device)
+        img = torch.from_numpy(img).to(self.device).float()  # 输入img为tensor形式
+        y = torch.tensor([self.tlabel], device=self.device)  # 具有真实标签的张量 默认为None
 
         if self.attacktype == 'untargeted':
-            img = fast_gradient_method(self.model, img, self.epsilon, np.inf, targeted=False) #非靶向 n_classes为int类型
+            img = fast_gradient_method(model_fn=self.model,
+                                       x=img,
+                                       eps=self.epsilon,
+                                       norm=np.inf,
+                                       clip_min=self.clip_min,
+                                       clip_max=self.clip_max,
+                                       y=None,
+                                       targeted=False,
+                                       sanity_checks=self.sanity_checks) #非靶向 n_classes为int类型
         else:
-            img = fast_gradient_method(self.model, img, self.epsilon, np.inf, targeted=True, y=y) #靶向 y带有真标签的张量
+            img = fast_gradient_method(model_fn=self.model,
+                                       x=img,
+                                       eps=self.epsilon,
+                                       norm=np.inf,
+                                       clip_min=self.clip_min,
+                                       clip_max=self.clip_max,
+                                       y=y,
+                                       targeted=True,
+                                       sanity_checks=self.sanity_checks) #靶向 y带有真标签的张量
 
         return img
