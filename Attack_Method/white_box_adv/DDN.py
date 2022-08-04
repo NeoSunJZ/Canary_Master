2d0ea59a5b727db4b8c2ef0b845f4d0b366320e0
tdlili <108248420+tdlili@users.noreply.github.com>
2022-08-04 15:59:47 +0800

    修改注释；增加修饰器函数

--- Attack_Method/white_box_adv/DDN.py
+++ Attack_Method/white_box_adv/DDN.py
@@ -7,21 +7,31 @@ from foolbox.criteria import TargetedMisclassification
 import eagerpy as ep
 
 from CANARY_SEFI.core.component.component_decorator import SEFIComponent
+from CANARY_SEFI.core.component.component_enum import ComponentType, ComponentConfigHandlerType
 
 sefi_component = SEFIComponent()
 
 @sefi_component.attacker_class(attack_name="DDN", perturbation_budget_var_name="epsilon")
+@sefi_component.config_params_handler(handler_target=ComponentType.ATTACK, name="DDN",
+                                      args_type=ComponentConfigHandlerType.ATTACK_PARAMS, use_default_handler=True,
+                                      params={
+                                          "attack_type": {"desc": "攻击类型(靶向(TARGETED) / 非靶向(UNTARGETED))", "type": "INT"},
+                                          "tlabel": {"desc": "靶向攻击目标标签(分类标签)(仅TARGETED时有效)", "type": "INT"},
+                                          "init_epsilon": {"desc": "范数(epsilon ball)初始值", "type": "FLOAT", "df_v": "1.0"},
+                                          "steps": {"desc": "优化步骤数", "type": "INT", "df_v": "100"},
+                                          "gamma": {"desc": "修改范数的因素(new_norm = norm * (1 + or - gamma))", "type": "FLOAT", "df_v": "0.05"}})
+
 class DDN():
     def __init__(self, model, epsilon=0.03, attacktype='untargeted', tlabel=1, init_epsilon=1.0, steps=50, gamma=6/25):
         self.model = model  # 待攻击的白盒模型
         self.epsilon = epsilon  # 以无穷范数作为约束，设置最大值
-        self.attacktype = attacktype  # 攻击类型：靶向 or 非靶向
-        self.tlabel = tlabel
+        self.attacktype = attacktype  # 攻击类型(靶向(TARGETED) / 非靶向(UNTARGETED)
+        self.tlabel = tlabel  # 靶向攻击目标标签(分类标签)(仅TARGETED时有效)
         self.label = -1
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        self.init_epsilon = init_epsilon  # 范数/epsilon ball初始值 浮点型
-        self.steps = steps  # 优化步骤数 整型
-        self.gamma = gamma  # 修改范数的因素：new_norm = norm * (1 + or - gamma) 浮点型
+        self.init_epsilon = init_epsilon  # 范数(epsilon ball)初始值
+        self.steps = steps  # 优化步骤数
+        self.gamma = gamma  # 修改范数的因素(new_norm = norm * (1 + or - gamma))
 
     @sefi_component.attack(name="DDN", is_inclass=True, support_model=["vision_transformer"])
     def attack(self, img, ori_label):
@@ -47,7 +57,7 @@ class DDN():
             raw, clipped, is_adv = attack(fmodel, img, ori_label, epsilons=self.epsilon)  # 模型、图像、真标签
             # raw正常攻击产生的对抗样本，clipped通过epsilons剪裁生成的对抗样本，is_adv每个样本的布尔值
         else:
-            criterion = TargetedMisclassification(target_classes=torch.tensor([self.tlabel] ), device=self.device)  # 参数为具有目标类的张量
+            criterion = TargetedMisclassification(target_classes=torch.tensor([self.tlabel]))  # 参数为具有目标类的张量
             raw, clipped, is_adv = attack(fmodel, img, ori_label, epsilons=self.epsilon, criterion=criterion)
 
         adv_img = raw.raw
