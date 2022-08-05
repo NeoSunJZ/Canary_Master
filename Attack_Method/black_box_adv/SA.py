f9b0272d858a3493afb9623dc0d1009e499cbbf0
tdlili <108248420+tdlili@users.noreply.github.com>
2022-08-05 10:17:28 +0800

    Update SA.py

--- Attack_Method/black_box_adv/SA.py
+++ Attack_Method/black_box_adv/SA.py
@@ -7,12 +7,24 @@ from foolbox.criteria import TargetedMisclassification
 import eagerpy as ep
 
 from CANARY_SEFI.core.component.component_decorator import SEFIComponent
+from CANARY_SEFI.core.component.component_enum import ComponentType, ComponentConfigHandlerType
 
 sefi_component = SEFIComponent()
 
 @sefi_component.attacker_class(attack_name="SA", perturbation_budget_var_name="epsilon")
+@sefi_component.config_params_handler(handler_target=ComponentType.ATTACK, name="SA",
+                                      args_type=ComponentConfigHandlerType.ATTACK_PARAMS, use_default_handler=True,
+                                      params={
+                                          "attack_type": {"desc": "攻击类型(靶向(TARGETED) / 非靶向(UNTARGETED))", "type": "INT"},
+                                          "tlabel": {"desc": "靶向攻击目标标签(分类标签)(仅TARGETED时有效)", "type": "INT"},
+                                          "max_translation": {"type": "FLOAT", "df_v": "3"},
+                                          "max_rotation": {"type": "FLOAT", "df_v": "30"},
+                                          "num_translations": {"type": "INT", "df_v": "5"},
+                                          "num_rotations": {"type": "INT", "df_v": "5"},
+                                          "grid_search": {"type": "BOOL", "df_v": "True"},
+                                          "random_steps": {"type": "INT", "df_v": "100"}})
 class SA():
-    def __init__(self, model, epsilon=0.03, attacktype='untargeted', tlabel=1, max_translation=3, max_rotation=30,
+    def __init__(self, model, epsilon=0.03, attacktype='UNTARGETED', tlabel=1, max_translation=3, max_rotation=30,
                  num_translations=5, num_rotations=5, grid_search=True, random_steps=100):
         self.model = model  # 待攻击的白盒模型
         self.epsilon = epsilon  # 以无穷范数作为约束，设置最大值
@@ -26,7 +38,7 @@ class SA():
         self.grid_search = grid_search  # 布尔型
         self.random_steps = random_steps  # 整型
 
-    @sefi_component.attack(name="SA", is_inclass=True, support_model=["vision_transformer"])
+    @sefi_component.attack(name="SA", is_inclass=True, support_model=["vision_transformer"], attack_type="BLACK_BOX")
     def attack(self, img, ori_label):
         # 模型预处理
         preprocessing = dict(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225], axis=-3)
@@ -47,7 +59,7 @@ class SA():
         attack = SpatialAttack(max_translation=self.max_translation, max_rotation=self.max_rotation, num_translations=self.num_translations,
                                num_rotations=self.num_rotations, grid_search=self.grid_search, random_steps=self.random_steps)
         self.epsilons = np.linspace(0.0, 0.005, num=20)
-        if self.attacktype == 'untargeted':
+        if self.attacktype == 'UNTARGETED':
             raw, clipped, is_adv = attack(fmodel, img, ori_label, epsilons=self.epsilon)  # 模型、图像、真标签
             # raw正常攻击产生的对抗样本，clipped通过epsilons剪裁生成的对抗样本，is_adv每个样本的布尔值
         else:
