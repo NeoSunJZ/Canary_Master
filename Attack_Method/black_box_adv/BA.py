3f904f4a055c89b1e3ab8e42f01a91b7bacc948d
tdlili <108248420+tdlili@users.noreply.github.com>
2022-08-05 10:17:07 +0800

    Update BA.py

--- Attack_Method/black_box_adv/BA.py
+++ Attack_Method/black_box_adv/BA.py
@@ -7,12 +7,26 @@ from foolbox.criteria import TargetedMisclassification
 import eagerpy as ep
 
 from CANARY_SEFI.core.component.component_decorator import SEFIComponent
+from CANARY_SEFI.core.component.component_enum import ComponentType, ComponentConfigHandlerType
 
 sefi_component = SEFIComponent()
 
 @sefi_component.attacker_class(attack_name="BA", perturbation_budget_var_name="epsilon")
+@sefi_component.config_params_handler(handler_target=ComponentType.ATTACK, name="BA",
+                                      args_type=ComponentConfigHandlerType.ATTACK_PARAMS, use_default_handler=True,
+                                      params={
+                                          "attack_type": {"desc": "攻击类型(靶向(TARGETED) / 非靶向(UNTARGETED))", "type": "INT"},
+                                          "tlabel": {"desc": "靶向攻击目标标签(分类标签)(仅TARGETED时有效)", "type": "INT"},
+                                          "init_attack": {"desc": "攻击用来寻找起点 仅当starting_points为None时才使用", "df_v": "None"},
+                                          "steps": {"desc": "要运行的最大步骤数，可能会在此之前收敛并停止", "type": "INT", "df_v": "25000"},
+                                          "spherical_step": {"desc": "正交（球形）补偿的初始步长", "type": "FLOAT", "df_v": "0.01"},
+                                          "source_step": {"desc": "迈向目标的步骤的初始步长", "type": "FLOAT", "df_v": "0.01"},
+                                          "source_step_convergance": {"desc": "设置停止条件的阈值，若在攻击期间source_step小于此值，则攻击已收敛并将停止", "type": "FLOAT", "df_v": "1e-07"},
+                                          "step_adaptation": {"desc": "步长乘以或除以的因子", "type": "FLOAT", "df_V": "1.5"},
+                                          "tensorboard": {"desc": "TensorBoard摘要的日志目录(禁用(False)/运行/CURRENT_DATETIME_HOSTNAME(Nnoe))", "type": "STR", "df_v": "False"},
+                                          "update_stats_every_k": {"type": "INT", "df_v": "10"}})
 class BA():
-    def __init__(self, model, epsilon=0.03, attacktype='untargeted', tlabel=1, init_attack=None, steps=50,
+    def __init__(self, model, epsilon=0.03, attacktype='UNTARGETED', tlabel=1, init_attack=None, steps=25000,
                  spherical_step=0.01, source_step=0.01, source_step_convergance=1e-07, step_adaptation=1.5,
                  tensorboard=False, update_stats_every_k=10):
         self.model = model  # 待攻击的白盒模型
@@ -23,13 +37,13 @@ class BA():
         self.steps = steps  # 要运行的最大步骤数，可能会在此之前收敛并停止 整型
         self.spherical_step = spherical_step  # 正交（球形）补偿的初始步长
         self.source_step = source_step  # 迈向目标的步骤的初始步长 浮点型
-        self.source_step_convergance = source_step_convergance  # 设置停止条件的阈值：如果在攻击期间source_step小于此值，则攻击已收敛并将停止
+        self.source_step_convergance = source_step_convergance  # 设置停止条件的阈值，若在攻击期间source_step小于此值，则攻击已收敛并将停止
         self.step_adaptation = step_adaptation  # 步长乘以或除以的因子 浮点型
-        self.tensorboard = tensorboard  # TensorBoard摘要的日志目录。如果为False，则TensorBoard摘要将要被禁用；如果为None,则将运行/CURRENT_DATETIME_HOSTNAME
+        self.tensorboard = tensorboard  # TensorBoard摘要的日志目录(禁用(False)/运行/CURRENT_DATETIME_HOSTNAME(Nnoe))
         self.update_stats_every_k =update_stats_every_k  # 整型
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-    @sefi_component.attack(name="BA", is_inclass=True, support_model=["vision_transformer"])
+    @sefi_component.attack(name="BA", is_inclass=True, support_model=["vision_transformer"], attack_type="BLACK_BOX")
     def attack(self, img, ori_label):
         # 模型预处理
         preprocessing = dict(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225], axis=-3)
@@ -51,7 +65,7 @@ class BA():
                                 source_step=self.source_step, source_step_convergance=self.source_step_convergance,
                                 step_adaptation=self.step_adaptation, tensorboard=self.tensorboard, update_stats_every_k=self.update_stats_every_k)
         self.epsilons = np.linspace(0.0, 0.005, num=20)
-        if self.attacktype == 'untargeted':
+        if self.attacktype == 'UNTARGETED':
             raw, clipped, is_adv = attack(fmodel, img, ori_label, epsilons=self.epsilon)  # 模型、图像、真标签
             # raw正常攻击产生的对抗样本，clipped通过epsilons剪裁生成的对抗样本，is_adv每个样本的布尔值
         else:
