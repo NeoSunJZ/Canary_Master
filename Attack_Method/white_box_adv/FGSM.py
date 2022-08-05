ab019a8c523fd77a9d7167cc22b5b59cdd7387f8
tdlili <108248420+tdlili@users.noreply.github.com>
2022-08-05 10:12:12 +0800

    修改注释；增加修饰器函数

--- Attack_Method/white_box_adv/FGSM.py
+++ Attack_Method/white_box_adv/FGSM.py
@@ -7,13 +7,20 @@ from foolbox.criteria import TargetedMisclassification
 import eagerpy as ep
 
 from CANARY_SEFI.core.component.component_decorator import SEFIComponent
+from CANARY_SEFI.core.component.component_enum import ComponentType, ComponentConfigHandlerType
 
 sefi_component = SEFIComponent()
 
 
 @sefi_component.attacker_class(attack_name="FGSM", perturbation_budget_var_name="epsilon")
+@sefi_component.config_params_handler(handler_target=ComponentType.ATTACK, name="FGSM",
+                                      args_type=ComponentConfigHandlerType.ATTACK_PARAMS, use_default_handler=True,
+                                      params={
+                                          "attack_type": {"desc": "攻击类型(靶向(TARGETED) / 非靶向(UNTARGETED))", "type": "INT"},
+                                          "tlabel": {"desc": "靶向攻击目标标签(分类标签)(仅TARGETED时有效)", "type": "INT"},
+                                          "random_start": {"desc": "控制是否在允许的epsilon ball内随机启动", "type": "BOOL", "df_v": "False"}})
 class FGSM():
-    def __init__(self, model, epsilon=0.03, attacktype='untargeted', tlabel=1, random_start=False):
+    def __init__(self, model, epsilon=0.03, attacktype='UNTARGETED', tlabel=1, random_start=False):
         self.model = model  # 待攻击的白盒模型
         self.epsilon = epsilon  # 以无穷范数作为约束，设置最大值
         self.attacktype = attacktype  # 攻击类型：靶向 or 非靶向
@@ -22,7 +29,7 @@ class FGSM():
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.random_start = random_start  # 控制是否在允许的epsilon ball内随机启动
 
-    @sefi_component.attack(name="FGSM", is_inclass=True, support_model=["vision_transformer"])
+    @sefi_component.attack(name="FGSM", is_inclass=True, support_model=["vision_transformer"], attack_type="WHITE_BOX")
     def attack(self, img, ori_label):
         # 模型预处理
         preprocessing = dict(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225], axis=-3)
@@ -42,7 +49,7 @@ class FGSM():
         # 实例化攻击类
         attack = LinfFastGradientAttack(random_start=self.random_start)
         self.epsilons = np.linspace(0.0, 0.005, num=20)
-        if self.attacktype == 'untargeted':
+        if self.attacktype == 'UNTARGETED':
             raw, clipped, is_adv = attack(fmodel, img, ori_label, epsilons=self.epsilon)  # 模型、图像、真标签
             # raw正常攻击产生的对抗样本，clipped通过epsilons剪裁生成的对抗样本，is_adv每个样本的布尔值
         else:
