b9c24eba20575b15459db0f9cd4aee4a25f957b3
tdlili <108248420+tdlili@users.noreply.github.com>
2022-08-11 17:06:50 +0800

    Update GA.py

--- Attack_Method/black_box_adv/GA.py
+++ Attack_Method/black_box_adv/GA.py
@@ -15,15 +15,15 @@ sefi_component = SEFIComponent()
 @sefi_component.config_params_handler(handler_target=ComponentType.ATTACK, name="GA",
                                       args_type=ComponentConfigHandlerType.ATTACK_PARAMS, use_default_handler=True,
                                       params={
-                                          "attack_type": {"desc": "攻击类型(靶向(TARGETED) / 非靶向(UNTARGETED))", "type": "INT"},
+                                          "attack_type": {"desc": "攻击类型", "type": "SELECT", "selector": [{"value": "TARGETED", "name": "靶向"}, {"value": "UNTARGETED", "name": "非靶向"}], "def": "TARGETED"},
                                           "tlabel": {"desc": "靶向攻击目标标签(分类标签)(仅TARGETED时有效)", "type": "INT"},
-                                          "steps": { "type": "INT", "df_v": "1000"},
-                                          "population": {"type": "INT", "df_v": "10"},
-                                          "mutation_probability": {"type": "FLOAT", "df_v": "0.10"},
-                                          "mutation_range": {"type": "FLOAT", "df_v": "0.15"},
-                                          "sampling_temperature": {"type": "FLOAT", "df_v": "0.3"},
-                                          "channel_axis": {"type": "INT", "df_v": "None"},
-                                          "reduced_dims": {"type": "Tuple[int,int]", "df_v": "None"}})
+                                          "steps": {"desc": "迭代攻击轮数", "type": "INT", "def": "1000"},
+                                          "population": {"desc": "种群", "type": "INT", "def": "10"},
+                                          "mutation_probability": {"desc": "突变概率", "type": "FLOAT", "def": "0.10"},
+                                          "mutation_range": {"desc": "突变范围", "type": "FLOAT", "def": "0.15"},
+                                          "sampling_temperature": {"desc": "采样温度", "type": "FLOAT", "def": "0.3"},
+                                          "channel_axis": {"desc": "通道轴", "type": "INT", "def": "None"},
+                                          "reduced_dims": {"type": "Tuple[int,int]", "def": "None"}})
 class GA():
     def __init__(self, model, epsilon=0.03, attacktype='TARGETED', tlabel=1, steps=1000, population=10, mutation_probability=0.10,
                  mutation_range=0.15, sampling_temperature=0.3, channel_axis=None, reduced_dims=None):
@@ -33,11 +33,11 @@ class GA():
         self.tlabel = tlabel
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.steps = steps  # 整型
-        self.population = population  # 整型
+        self.population = population  # 种群 整型
         self.mutation_probability = mutation_probability  # 突变概率 浮点型
         self.mutation_range = mutation_range  # 突变范围 浮点型
         self.sampling_temperature = sampling_temperature  # 采样温度 浮点型
-        self.channel_axis = channel_axis  # 整型
+        self.channel_axis = channel_axis  # 通道轴 整型
         self.reduced_dims = reduced_dims  # 元组[整型，整型]
 
     @sefi_component.attack(name="GA", is_inclass=True, support_model=["vision_transformer"], attack_type="BLACK_BOX")
@@ -59,12 +59,16 @@ class GA():
 
         # 实例化攻击类
         attack = GenAttack(steps=self.steps, population=self.population, mutation_probability=self.mutation_probability,
-                           mutation_range = self.mutation_range, sampling_temperature=self.sampling_temperature,
+                           mutation_range=self.mutation_range, sampling_temperature=self.sampling_temperature,
                            channel_axis=self.channel_axis, reduced_dims=self.reduced_dims)
         self.epsilons = np.linspace(0.0, 0.005, num=20)
-        criterion = TargetedMisclassification(target_classes=torch.tensor([self.tlabel]))  # 参数为具有目标类的张量
-        raw, clipped, is_adv = attack(model=fmodel, inputs=img, epsilons=self.epsilon, criterion=criterion)
-        # raw正常攻击产生的对抗样本，clipped通过epsilons剪裁生成的对抗样本，is_adv每个样本的布尔值
+        if self.attacktype == 'UNTARGETED':
+            raw, clipped, is_adv = attack(fmodel, img, ori_label, epsilons=self.epsilon)  # 模型、图像、真标签
+            # raw正常攻击产生的对抗样本，clipped通过epsilons剪裁生成的对抗样本，is_adv每个样本的布尔值
+        else:
+            criterion = TargetedMisclassification(target_classes=torch.tensor([self.tlabel]))  # 参数为具有目标类的张量
+            raw, clipped, is_adv = attack(fmodel, img, criterion=criterion, epsilons=self.epsilon)
+
         adv_img = raw.raw
         # 由EagerPy张量转化为Native张量
 
