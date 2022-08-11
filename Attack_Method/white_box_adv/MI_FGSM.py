d21e13e9699550561e932ea9c19294e2cdc326db
tdlili <108248420+tdlili@users.noreply.github.com>
2022-08-11 17:03:15 +0800

    Update MI_FGSM.py

--- Attack_Method/white_box_adv/MI_FGSM.py
+++ Attack_Method/white_box_adv/MI_FGSM.py
@@ -1,16 +1,25 @@
 import torch
-import numpy as np
-from PIL import Image
 from tqdm import tqdm
 
 from CANARY_SEFI.core.component.component_decorator import SEFIComponent
+from CANARY_SEFI.core.component.component_enum import ComponentType, ComponentConfigHandlerType
 
 sefi_component = SEFIComponent()
 
 
 @sefi_component.attacker_class(attack_name="MI-FGSM", perturbation_budget_var_name="epsilon")
+@sefi_component.config_params_handler(handler_target=ComponentType.ATTACK, name="MI-FGSM",
+                                      args_type=ComponentConfigHandlerType.ATTACK_PARAMS, use_default_handler=True,
+                                      params={
+                                          "T": {"desc": "迭代攻击轮数", "type": "INT", "def": "50"},
+                                          "epsilon": {"desc": "以无穷范数作为约束，设置最大值", "type": "FLOAT", "def": "0.2"},
+                                          "pixel_min": {"desc": "像素值的下限", "type": "FLOAT", "def": "-3"},
+                                          "pixel_max": {"desc": "像素值的上限", "type": "FLOAT", "def": "3"},
+                                          "alpha": {"deac": "每一轮迭代攻击的步长", "type": "FLOAT", "def": "6/25"},
+                                          "attack_type": {"desc": "攻击类型", "type": "SELECT", "selector": [{"value": "TARGETED", "name": "靶向"}, {"value": "UNTARGETED", "name": "非靶向"}], "def": "TARGETED"},
+                                          "tlabel": {"desc": "靶向攻击目标标签(分类标签)(仅TARGETED时有效)", "type": "INT"}})
 class MI_FGSM():
-    def __init__(self, model, T=50, epsilon=0.2, pixel_min=-3, pixel_max=3, alpha=6 / 25, attacktype='untargeted',
+    def __init__(self, model, T=50, epsilon=0.2, pixel_min=-3, pixel_max=3, alpha=6 / 25, attacktype='UNTARGETED',
                  tlabel=-1):
         self.model = model  # 待攻击的白盒模型
         self.T = T  # 迭代攻击轮数
@@ -58,7 +67,7 @@ class MI_FGSM():
         x.data = self.clip_value(x, ori_x)
         return x.data, sum_grad
 
-    @sefi_component.attack(name="MI-FGSM", is_inclass=True, support_model=["vision_transformer"])
+    @sefi_component.attack(name="MI-FGSM", is_inclass=True, support_model=["vision_transformer"], attack_type="WHITE_BOX")
     def attack(self, img, ori_label):
         img = torch.from_numpy(img).to(self.device).float()
         # 定义图片可获取梯度，设置计算图叶子节点
@@ -68,7 +77,7 @@ class MI_FGSM():
         # 定义累计梯度
         sum_grad = 0
         # 迭代攻击开始
-        if self.attacktype == 'untargeted':
+        if self.attacktype == 'UNTARGETED':
             # self.label = np.argmax(self.model(img).data.to('cpu').numpy())
             self.label = ori_label
 
