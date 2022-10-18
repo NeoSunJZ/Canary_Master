4af145748e274f6d6b9febeb567007d457b563f9
unknown <474743988@qq.com>
2022-10-18 20:55:53 +0800

    修正FGSM的实现

--- Attack_Method/white_box_adv/FGSM.py
+++ Attack_Method/white_box_adv/FGSM.py
@@ -3,7 +3,6 @@ import numpy as np
 
 from foolbox.attacks import LinfFastGradientAttack
 import foolbox as fb
-from foolbox.criteria import TargetedMisclassification
 import eagerpy as ep
 
 from CANARY_SEFI.core.component.component_decorator import SEFIComponent
@@ -18,16 +17,13 @@ sefi_component = SEFIComponent()
                                       params={
                                           "clip_min": {"desc": "对抗样本像素上界(与模型相关)", "type": "FLOAT", "required": "true"},
                                           "clip_max": {"desc": "对抗样本像素下界(与模型相关)", "type": "FLOAT", "required": "true"},
-                                          "epsilon": {"desc": "对抗样本与原始输入图片的最大变化", "type": "FLOAT", "def": "0.03"},
+                                          "epsilon": {"desc": "对抗样本与原始输入图片的最大变化", "type": "FLOAT", "def": "0.25"},
                                           "random_start": {"desc": "是否允许在ε球的空间范围初始化", "type": "BOOLEAN", "def": "False"},
-                                          "attack_type": {"desc": "攻击类型", "type": "SELECT", "selector": [{"value": "TARGETED", "name": "靶向"},{"value": "UNTARGETED", "name": "非靶向"}], "required": "true"},
-                                          "tlabel": {"desc": "靶向攻击目标标签(分类标签)(仅TARGETED时有效)", "type": "INT"}})
+                                          })
 class FGSM():
-    def __init__(self, model, clip_min=0, clip_max=1, epsilon=0.03, attack_type='UNTARGETED', tlabel=-1, random_start=False):
+    def __init__(self, model, clip_min=0, clip_max=1, epsilon=0.25, random_start=False):
         self.model = model  # 待攻击的白盒模型
         self.epsilon = epsilon  # 以无穷范数作为约束，设置最大值
-        self.attack_type = attack_type  # 攻击类型：靶向 or 非靶向
-        self.tlabel = tlabel
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.random_start = random_start  # 控制是否在允许的epsilon ball内随机启动
         self.clip_min = clip_min
@@ -43,15 +39,8 @@ class FGSM():
         img = ep.astensor(img)
         # 实例化攻击类
         attack = LinfFastGradientAttack(random_start=self.random_start)
-        if self.attack_type == 'UNTARGETED':
-            raw, clipped, is_adv = attack(self.model, img, ori_label, epsilons=self.epsilon)  # 模型、图像、真标签
-            # raw正常攻击产生的对抗样本，clipped通过epsilons剪裁生成的对抗样本，is_adv每个样本的布尔值
-        else:
-            criterion = TargetedMisclassification(target_classes=torch.tensor([self.tlabel]),
-                                                  device=self.device)  # 参数为具有目标类的张量
-            raw, clipped, is_adv = attack(self.model, img, ori_label, epsilons=self.epsilon, criterion=criterion)
-
+        raw, clipped, is_adv = attack(self.model, img, ori_label, epsilons=self.epsilon)  # 模型、图像、真标签
+        # raw正常攻击产生的对抗样本，clipped通过epsilons剪裁生成的对抗样本，is_adv每个样本的布尔值
         adv_img = raw.raw
         # 由EagerPy张量转化为Native张量
-
-        return adv_img
+        return adv_img.cpu().numpy()
