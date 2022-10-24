1c3e469e3863240ba437a2fcfd8c16e91b205e5f
NeoSunJz <neosunjz@qq.com>
2022-10-24 19:40:28 +0800

    修改了本地运行文件

--- run_local.py
+++ run_local.py
@@ -1,31 +1,32 @@
 from CANARY_SEFI.batch_manager import batch_manager
 from CANARY_SEFI.core.component.component_manager import SEFI_component_manager
-
-# 模型
-# VTI
-from CANARY_SEFI.core.function.helper.recovery import global_recovery
 from CANARY_SEFI.service.security_evaluation import SecurityEvaluation
-from Model.Vision_Transformer import sefi_component as vision_transformer_model
-
-SEFI_component_manager.add(vision_transformer_model)
+# 模型
 # Alexnet
-from Model.Alexnet_ImageNet import sefi_component as alexnet_model
+from Model.ImageNet.Alexnet_ImageNet import sefi_component as alexnet_model
+# ConvNext
+from Model.ImageNet.ConvNeXt_ImageNet import sefi_component as convnext_model
 
 SEFI_component_manager.add(alexnet_model)
-# VGG16
-from Model.VGG16_ImageNet import sefi_component as vgg_16_model
+SEFI_component_manager.add(convnext_model)
 
-SEFI_component_manager.add(vgg_16_model)
+from Model.ImageNet.common import sefi_component as common
+SEFI_component_manager.add(common)
 
 # 攻击方案
 # CW
 from Attack_Method.white_box_adv.CW import sefi_component as cw_attacker
-
-SEFI_component_manager.add(cw_attacker)
 # MI-FGSM
 from Attack_Method.white_box_adv.MI_FGSM import sefi_component as mi_fgsm_attacker
+# UAP
+from Attack_Method.white_box_adv.UAP import sefi_component as uap_attacker
+# DeepFool
+from Attack_Method.white_box_adv.DeepFool import sefi_component as deepfool_attacker
 
+SEFI_component_manager.add(cw_attacker)
 SEFI_component_manager.add(mi_fgsm_attacker)
+SEFI_component_manager.add(uap_attacker)
+SEFI_component_manager.add(deepfool_attacker)
 
 # 数据集
 # IMAGENET2012
@@ -34,38 +35,89 @@ from Dataset.ImageNet2012.dataset_loader import sefi_component as imgnet2012_dat
 SEFI_component_manager.add(imgnet2012_dataset)
 
 if __name__ == "__main__":
-
-    config = {"dataset_size": 3,"dataset": "ILSVRC-2012",
+    config = {"dataset_size": 2,"dataset": "ILSVRC-2012",
               "model_list": [
-                  "Alexnet(ImageNet)",
-                  # "VGG-16(ImageNet)"
+                  "ConvNext(ImageNet)",
               ],
-              "model_config": {"Alexnet(ImageNet)": {}, "VGG-16(ImageNet)": {}},
-              "img_proc_config": {"Alexnet(ImageNet)": {}, "VGG-16(ImageNet)": {}},
+              "model_config": {"ConvNext(ImageNet)": {},},
+              "img_proc_config": {"ConvNext(ImageNet)": {},},
               "attacker_list": {
-                  # "CW": [
-                  #     "Alexnet(ImageNet)",
-                  #     "VGG-16(ImageNet)"
-                  # ],
                   "MI-FGSM": [
-                      "Alexnet(ImageNet)",
-                      # "VGG-16(ImageNet)"
+                      "ConvNext(ImageNet)",
                   ],
               },
-              # "transfer_attack_test_mode": "SELF_CROSS",
               "transfer_attack_test_mode": "NOT",
               "transfer_attack_test_on_model_list": {},
               "attacker_config": {
-                  # "CW": {"classes": 1000, "lr": 0.005, "confidence": 0, "clip_min": -3, "clip_max": 3,
-                  #        "initial_const": 0.01,
-                  #        "binary_search_steps": 5, "max_iterations": 1000, "attack_type": "UNTARGETED", "tlabel": None},
-                  "MI-FGSM": {"alpha": 0.005, "epsilon": 0.2, "pixel_min": -3, "pixel_max": 3, "T": 1000,
+                  "MI-FGSM": {"alpha": 0.001, "epsilon": None, "pixel_min": 0, "pixel_max": 1, "T": 1000,
                               "attack_type": "UNTARGETED", "tlabel": None}
+                  # "UAP": {"num_classes":1000,
+                  #         "xi":10 / 255.0,
+                  #         "p":"l-inf",
+                  #         "overshoot":0.02,
+                  #         "delta":0.2,
+                  #         "max_iter_df":10
+                  #         }
+                  }
               }
-              }
+    # config = {"dataset_size": 2,"dataset": "ILSVRC-2012",
+    #           "model_list": [
+    #               "Alexnet(ImageNet)",
+    #           ],
+    #           "model_config": {"Alexnet(ImageNet)": {},},
+    #           "img_proc_config": {"Alexnet(ImageNet)": {},},
+    #           "attacker_list": {
+    #               "DeepFool": [
+    #                   "Alexnet(ImageNet)",
+    #               ],
+    #           },
+    #           "transfer_attack_test_mode": "NOT",
+    #           "transfer_attack_test_on_model_list": {},
+    #           "attacker_config": {
+    #               "DeepFool": {
+    #                   "pixel_min": 0,
+    #                   "pixel_max": 1,
+    #                   "num_classes":1000,
+    #                   "p":"l-2",
+    #                   "overshoot":0.02,
+    #                   "max_iter":10
+    #               }}
+    #           }
     batch_manager.init_batch(show_logo=True)
     security_evaluation = SecurityEvaluation(config)
     security_evaluation.attack_full_test(use_img_file=True, use_raw_nparray_data=True)
+
+    # config = {"dataset_size": 3,"dataset": "ILSVRC-2012",
+    #           "model_list": [
+    #               "Alexnet(ImageNet)",
+    #               # "VGG-16(ImageNet)"
+    #           ],
+    #           "model_config": {"Alexnet(ImageNet)": {}, "VGG-16(ImageNet)": {}},
+    #           "img_proc_config": {"Alexnet(ImageNet)": {}, "VGG-16(ImageNet)": {}},
+    #           "attacker_list": {
+    #               # "CW": [
+    #               #     "Alexnet(ImageNet)",
+    #               #     "VGG-16(ImageNet)"
+    #               # ],
+    #               "MI-FGSM": [
+    #                   "Alexnet(ImageNet)",
+    #                   # "VGG-16(ImageNet)"
+    #               ],
+    #           },
+    #           # "transfer_attack_test_mode": "SELF_CROSS",
+    #           "transfer_attack_test_mode": "NOT",
+    #           "transfer_attack_test_on_model_list": {},
+    #           "attacker_config": {
+    #               # "CW": {"classes": 1000, "lr": 0.005, "confidence": 0, "clip_min": -3, "clip_max": 3,
+    #               #        "initial_const": 0.01,
+    #               #        "binary_search_steps": 5, "max_iterations": 1000, "attack_type": "UNTARGETED", "tlabel": None},
+    #               "MI-FGSM": {"alpha": 0.005, "epsilon": 0.2, "pixel_min": -3, "pixel_max": 3, "T": 1000,
+    #                           "attack_type": "UNTARGETED", "tlabel": None}
+    #           }
+    #           }
+    # batch_manager.init_batch(show_logo=True)
+    # security_evaluation = SecurityEvaluation(config)
+    # security_evaluation.attack_full_test(use_img_file=True, use_raw_nparray_data=True)
 
     # config = {"dataset_size": 150, "dataset": "ILSVRC-2012",
     #           "model_list": ["Alexnet(ImageNet)", "VGG-16(ImageNet)"],
