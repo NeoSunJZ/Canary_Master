b4addd57394ebe83a8003b501713e65be6489b12
Sumorile <474743988@qq.com>
2022-10-24 22:08:01 +0800

    填充ImageNet各模型

---
+++ Model/ImageNet/ShuffleNetV2_ImageNet.py
@@ -0,0 +1,19 @@
+import torch
+from torch import nn
+from torchvision import models
+from torchvision.models import ShuffleNet_V2_X2_0_Weights
+from torchvision.transforms import Normalize
+
+from CANARY_SEFI.core.component.component_decorator import SEFIComponent
+
+sefi_component = SEFIComponent()
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
+@sefi_component.model(name="ShuffleNetV2(ImageNet)")
+def create_model():
+    norm_layer = Normalize(mean=[0.485, 0.456, 0.406], std=[0.229,0.224,0.225])
+    densenet_model = nn.Sequential(
+        norm_layer,
+        models.shufflenet_v2_x2_0(weights=ShuffleNet_V2_X2_0_Weights.IMAGENET1K_V1)).to(device).eval()
+    return densenet_model.eval()
---
+++ Model/ImageNet/VisionTransformer_ImageNet.py
@@ -0,0 +1,19 @@
+import torch
+from torch import nn
+from torchvision import models
+from torchvision.models import ViT_H_14_Weights
+from torchvision.transforms import Normalize
+
+from CANARY_SEFI.core.component.component_decorator import SEFIComponent
+
+sefi_component = SEFIComponent()
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
+@sefi_component.model(name="ViT(ImageNet)")
+def create_model():
+    norm_layer = Normalize(mean=[0.485, 0.456, 0.406], std=[0.229,0.224,0.225])
+    densenet_model = nn.Sequential(
+        norm_layer,
+        models.vit_h_14(weights=ViT_H_14_Weights.IMAGENET1K_SWAG_E2E_V1)).to(device).eval()
+    return densenet_model.eval()
