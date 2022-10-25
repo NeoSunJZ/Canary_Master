d2c481625170a72f4c493d6b24e895cc39c3062c
Sumorile <474743988@qq.com>
2022-10-25 14:40:05 +0800

    修改RegNet模型，原有的太大了

--- Model/ImageNet/RegNet_ImageNet.py
+++ Model/ImageNet/RegNet_ImageNet.py
@@ -1,7 +1,7 @@
 import torch
 from torch import nn
 from torchvision import models
-from torchvision.models import RegNet_Y_128GF_Weights
+from torchvision.models import RegNet_Y_8GF_Weights
 from torchvision.transforms import Normalize
 
 from CANARY_SEFI.core.component.component_decorator import SEFIComponent
@@ -15,5 +15,5 @@ def create_model():
     norm_layer = Normalize(mean=[0.485, 0.456, 0.406], std=[0.229,0.224,0.225])
     densenet_model = nn.Sequential(
         norm_layer,
-        models.regnet_y_128gf(weights=RegNet_Y_128GF_Weights.IMAGENET1K_SWAG_E2E_V1)).to(device).eval()
+        models.regnet_y_8gf(weights=RegNet_Y_8GF_Weights.IMAGENET1K_V1)).to(device).eval()
     return densenet_model.eval()
