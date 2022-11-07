1d5727835110e9d25105bc1d0ae2043341167fd9
NeoSunJz <neosunjz@qq.com>
2022-11-07 16:55:37 +0800

    修复了部分存在设备选择故障的集成模型

--- Model/ImageNet/Alexnet_ImageNet.py
+++ Model/ImageNet/Alexnet_ImageNet.py
@@ -11,10 +11,11 @@ sefi_component = SEFIComponent()
 
 @sefi_component.model(name="Alexnet(ImageNet)")
 def create_model(run_device=None):
-    run_device = run_device if run_device is None else ('cuda' if torch.cuda.is_available() else 'cpu')
+    run_device = run_device if run_device is not None else ('cuda' if torch.cuda.is_available() else 'cpu')
+    print(run_device)
     norm_layer = Normalize(mean=[0.485, 0.456, 0.406], std=[0.229,0.224,0.225])
     alexnet_model = nn.Sequential(
         norm_layer,
         models.alexnet(weights=AlexNet_Weights.IMAGENET1K_V1)
     ).to(run_device).eval()
-    return alexnet_model.eval()
+    return alexnet_model
