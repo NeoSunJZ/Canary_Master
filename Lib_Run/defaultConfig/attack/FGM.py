71937acd06d255c208c59258cb37a696b21adc77
NeoSunJZ <neosunjz@qq.com>
2023-02-02 14:06:54 +0800

    规范代码结构

---
+++ Lib_Run/defaultConfig/attack/FGM.py
@@ -0,0 +1,12 @@
+name = "FGM"
+object_list = ["FGM"]
+config = {
+    "attacker_config": {
+        "FGM": {
+            "clip_min": 0,
+            "clip_max": 1,
+            "epsilon": 16 / 255,
+            "attack_type": "UNTARGETED",
+        }
+    }
+}
