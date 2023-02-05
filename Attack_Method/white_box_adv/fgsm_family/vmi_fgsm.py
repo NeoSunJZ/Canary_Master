6c9d3aa92a5fe06f4c362e9b5b4ee9a2b1067202
BearFromeAncient <wqxiong1211@163.com>
2023-02-05 13:02:27 +0800

    添加攻击类型

--- Attack_Method/white_box_adv/fgsm_family/vmi_fgsm.py
+++ Attack_Method/white_box_adv/fgsm_family/vmi_fgsm.py
@@ -17,7 +17,7 @@ sefi_component = SEFIComponent()
                                       })
 
 class VMI_FGSM():
-    def __init__(self, model, run_device, clip_min=0, clip_max=1, T=50, epsilon=0.03):
+    def __init__(self, model, run_device, attack_type='TARGETED', tlabel=1, clip_min=0, clip_max=1, T=50, epsilon=0.03):
         self.model = model  # 待攻击的白盒模型
         self.clip_min = clip_min  # 对抗性示例组件的最小浮点值
         self.clip_max = clip_max  # 对抗性示例组件的最大浮点值
@@ -26,39 +26,41 @@ class VMI_FGSM():
         self.T = T  #迭代攻击次数
         self.loss = torch.nn.CrossEntropyLoss()  #定义loss
 
-    @sefi_component.attack(name="VMI_FGSM", is_inclass=True, support_model=[], attack_type="WHITE_BOX")
-    def attack(self, img, label):
+        self.attack_type = attack_type
+        self.target_label = tlabel
+
+    def vmi_fgsm_attack(self, x, y, targeted):
         # 将图像放置GPU中
-        img = img.cuda()
+        x = x.cuda()
         # 将label放置GPU中
-        label = np.array(label)
-        label = torch.LongTensor(label).to(self.device)
+        y = np.array(y)
+        y = torch.LongTensor(y).to(self.device)
         # 复制原始图像信息
-        ori = img.clone()
+        ori = x.clone()
         # 定义图像可获取梯度 1 224 224 3 RGB tensor
-        img.requires_grad = True
+        x.requires_grad = True
         # 定义累计梯度
         sum_grad = 0
         # !VMI 定义vt
         variance = 0
         # 迭代攻击
         for iter in range(self.T):
-            print(iter)
-            # 记录总输出，求和形式
-            output = 0
+            #print(iter)
             # 模型预测
             self.model.zero_grad()
-            output = self.model(img)
+            output = self.model(x)
             # 计算loss
-            loss = self.loss(output, label)
+            loss = self.loss(output, y)
+            if targeted:
+                loss = -loss
             # 反向传播
             loss.backward()
             # 得到图像梯度
-            grad = img.grad.data
+            grad = x.grad.data
             # !VMI
             new_grad = grad
             # 图像梯度清零
-            img.grad = None
+            x.grad = None
             # !VMI-FGSM
             # 定义本轮梯度与var相加
             current_grad = grad + variance
@@ -66,24 +68,37 @@ class VMI_FGSM():
             # 更新累计梯度
             sum_grad = grad
             # 计算V（x）。由于输入空间的连续性，无法直接得到前一项，因此可以采样N个样本来近似：
-            global_grad = torch.zeros_like(img, requires_grad=False)
+            global_grad = torch.zeros_like(x, requires_grad=False)
             for _ in range(20):
-                sample = img.clone().detach()
+                sample = x.clone().detach()
                 sample.requires_grad = True
-                rd = (torch.rand_like(img) * 2 - 1) * 1.5 * self.epsilon
+                rd = (torch.rand_like(x) * 2 - 1) * 1.5 * self.epsilon
                 sample = sample + rd.cuda()
                 self.model.zero_grad()
                 outputs_sample = self.model(sample)
-                loss_sample = torch.nn.functional.cross_entropy(outputs_sample, label)
+                if targeted:
+                    loss_sample = -torch.nn.functional.cross_entropy(outputs_sample, y)
+                else:
+                    loss_sample = torch.nn.functional.cross_entropy(outputs_sample, y)
                 global_grad += torch.autograd.grad(loss_sample, sample, grad_outputs=None, only_inputs=True)[0]
 
             variance = global_grad / (20 * 1.0) - new_grad
 
             # 更新图像像素 -1 1 0-255
-            img.data = img.data + ((self.epsilon * 2) / self.T) * torch.sign(grad)  # torch.clamp(grad,-2,2)
+            x.data = x.data + ((self.epsilon * 2) / self.T) * torch.sign(grad)  # torch.clamp(grad,-2,2)
             # 限制L无穷扰动
-            img.data = torch.clamp(img.data - ori.data, -self.epsilon, self.epsilon) + ori.data
+            x.data = torch.clamp(x.data - ori.data, -self.epsilon, self.epsilon) + ori.data
             # 限制图像像素范围
-            img.data = torch.clamp(img.data, 0, 255)
+            x.data = torch.clamp(x.data, 0, 255)
 
-        return img
+        return x
+
+    @sefi_component.attack(name="VMI_FGSM", is_inclass=True, support_model=[], attack_type="WHITE_BOX")
+    def attack(self, imgs, ori_labels, target_labels=None):
+        if self.attack_type == "UNTARGETED":
+            adv_img = self.vmi_fgsm_attack(x=imgs, y=ori_labels, targeted=False)
+        else:
+            batch_size = imgs.shape[0]
+            target_labels = (np.repeat(self.target_label, batch_size)) if target_labels is None else target_labels
+            adv_img = self.vmi_fgsm_attack(x=imgs, y=target_labels, targeted=True)
+        return adv_img
