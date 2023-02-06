8bc71cb2d7bec88f30abd676e2d953822da7042d
qiuzhi <2256599860@qq.com>
2023-02-06 22:47:42 +0800

    [已完成]PNA_NIM的集成与测试

---
+++ Lib_Run/defaultConfig/attack/L_BFGS.py
@@ -0,0 +1,13 @@
+name = "L_BFGS"
+object_list = ["L_BFGS"]
+config = {
+    "attacker_config": {
+        "L_BFGS": {
+            "clip_min": 0.0,
+            "clip_max": 1.0,
+            "steps": 10,
+            "attack_type": 'TARGETED',
+            "tlabel": 20,
+        }
+    }
+}
