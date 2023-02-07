5e3ed87d889173c47781178fdec02517b462a5e1
qiuzhi <2256599860@qq.com>
2023-02-07 23:42:19 +0800

    完成了PNA_NIM的测试

---
+++ Lib_Run/defaultConfig/attack/PNA_NIM.py
@@ -0,0 +1,13 @@
+name = "PNA_NIM"
+object_list = ["PNA_NIM"]
+config = {
+    "attacker_config": {
+        "PNA_NIM": {
+            "attack_type": 'UNTARGETED',
+            "clip_min": 0.0,
+            "clip_max": 1.0,
+            "T": 500,
+            "epsilon": 16/256,
+        }
+    }
+}
