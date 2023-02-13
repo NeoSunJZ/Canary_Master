02271bd9db448ec480f4275db716930353f7b426
NeoSunJZ <neosunjz@qq.com>
2023-02-13 21:13:11 +0800

    修复了ADVGAN代码彻底编写错误的问题（历史代码为完全错误的）

---
+++ Attack_Method/black_box_adv/adv_gan/adv_gan_core.py
@@ -0,0 +1,159 @@
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
+from tqdm import tqdm
+
+from Attack_Method.black_box_adv.adv_gan import models
+
+
+# custom weights initialization called on netG and netD
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
+
+
+class AdvGAN_Attack:
+    def __init__(self,
+                 device,
+                 model,
+                 model_num_labels,
+                 image_nc,
+                 box_min,
+                 box_max):
+        output_nc = image_nc
+        self.device = device
+        self.model_num_labels = model_num_labels
+        self.model = model
+        self.input_nc = image_nc
+        self.output_nc = output_nc
+        self.box_min = box_min
+        self.box_max = box_max
+
+        self.gen_input_nc = image_nc
+        self.netG = models.Generator(self.gen_input_nc, image_nc).to(device)
+        self.netDisc = models.Discriminator(image_nc).to(device)
+
+        # initialize all weights
+        self.netG.apply(weights_init)
+        self.netDisc.apply(weights_init)
+
+        # initialize optimizers
+        self.optimizer_G = torch.optim.Adam(self.netG.parameters(),
+                                            lr=0.001)
+        self.optimizer_D = torch.optim.Adam(self.netDisc.parameters(),
+                                            lr=0.001)
+
+    def train_batch(self, x, labels):
+        # optimize D
+        for i in range(1):
+            perturbation = self.netG(x)
+
+            # add a clipping trick
+            adv_images = torch.clamp(perturbation, -0.3, 0.3) + x
+            adv_images = torch.clamp(adv_images, self.box_min, self.box_max)
+
+            self.optimizer_D.zero_grad()
+            pred_real = self.netDisc(x)
+            loss_D_real = F.mse_loss(pred_real, torch.ones_like(pred_real, device=self.device))
+            loss_D_real.backward()
+
+            pred_fake = self.netDisc(adv_images.detach())
+            loss_D_fake = F.mse_loss(pred_fake, torch.zeros_like(pred_fake, device=self.device))
+            loss_D_fake.backward()
+            loss_D_GAN = loss_D_fake + loss_D_real
+            self.optimizer_D.step()
+
+        # optimize G
+        for i in range(1):
+            self.optimizer_G.zero_grad()
+
+            # cal G's loss in GAN
+            pred_fake = self.netDisc(adv_images)
+            loss_G_fake = F.mse_loss(pred_fake, torch.ones_like(pred_fake, device=self.device))
+            loss_G_fake.backward(retain_graph=True)
+
+            # calculate perturbation norm
+            C = 0.1
+            loss_perturb = torch.mean(torch.norm(perturbation.view(perturbation.shape[0], -1), 2, dim=1))
+            # loss_perturb = torch.max(loss_perturb - C, torch.zeros(1, device=self.device))
+
+            # cal adv loss
+            logits_model = self.model(adv_images)
+            probs_model = F.softmax(logits_model, dim=1)
+            onehot_labels = torch.eye(self.model_num_labels, device=self.device)[labels]
+
+            # C&W loss function
+            real = torch.sum(onehot_labels * probs_model, dim=1)
+            other, _ = torch.max((1 - onehot_labels) * probs_model - onehot_labels * 10000, dim=1)
+            zeros = torch.zeros_like(other)
+            loss_adv = torch.max(real - other, zeros)
+            loss_adv = torch.sum(loss_adv)
+
+            # maximize cross_entropy loss
+            # loss_adv = -F.mse_loss(logits_model, onehot_labels
+            # loss_adv = - F.cross_entropy(logits_model, labels
+
+            adv_lambda = 10
+            pert_lambda = 1
+            loss_G = adv_lambda * loss_adv + pert_lambda * loss_perturb
+            loss_G.backward()
+            self.optimizer_G.step()
+
+        return loss_D_GAN.item(), loss_G_fake.item(), loss_perturb.item(), loss_adv.item()
+
+    def train(self, dataset, batch_size, epochs, netG_file_name):
+        for epoch in range(1, epochs+1):
+
+            if epoch == 50:
+                self.optimizer_G = torch.optim.Adam(self.netG.parameters(),
+                                                    lr=0.0001)
+                self.optimizer_D = torch.optim.Adam(self.netDisc.parameters(),
+                                                    lr=0.0001)
+            if epoch == 80:
+                self.optimizer_G = torch.optim.Adam(self.netG.parameters(),
+                                                    lr=0.00001)
+                self.optimizer_D = torch.optim.Adam(self.netDisc.parameters(),
+                                                    lr=0.00001)
+            loss_D_sum = 0
+            loss_G_fake_sum = 0
+            loss_perturb_sum = 0
+            loss_adv_sum = 0
+
+            i = 0
+            images, labels = None, None
+
+            with tqdm(total=int(len(dataset)/batch_size), desc="Load Dataset", ncols=120) as bar:
+                for cur_img in dataset:
+                    image = cur_img[0]
+                    label = torch.tensor(cur_img[1]).to(self.device)
+                    i += 1
+
+                    image = torch.unsqueeze(image, dim=0)
+                    label = torch.unsqueeze(label, dim=0)
+
+                    images = image if images is None else torch.cat((images, image), dim=0)
+                    labels = label if labels is None else torch.cat((labels, label), dim=0)
+                    if i % batch_size == 0:
+                        loss_D_batch, loss_G_fake_batch, loss_perturb_batch, loss_adv_batch = \
+                            self.train_batch(images, labels)
+                        loss_D_sum += loss_D_batch
+                        loss_G_fake_sum += loss_G_fake_batch
+                        loss_perturb_sum += loss_perturb_batch
+                        loss_adv_sum += loss_adv_batch
+
+                        bar.update(1)
+
+                        images, labels = None, None
+
+                # print statistics
+                num_batch = len(dataset)
+                print("epoch %d:\nloss_D: %.3f, loss_G_fake: %.3f,\
+                \nloss_perturb: %.3f, loss_adv: %.3f, \n" %
+                      (epoch, loss_D_sum/num_batch, loss_G_fake_sum/num_batch,
+                       loss_perturb_sum/num_batch, loss_adv_sum/num_batch))
+
+        torch.save(self.netG.state_dict(), netG_file_name)
