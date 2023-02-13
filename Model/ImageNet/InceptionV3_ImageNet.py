5e40de38c698e8f8a933edb37dbeec73a7a63697
NeoSunJZ <neosunjz@qq.com>
2023-02-13 21:18:02 +0800

    修正了语法问题

--- Model/ImageNet/InceptionV3_ImageNet.py
+++ Model/ImageNet/InceptionV3_ImageNet.py
@@ -12,7 +12,7 @@ sefi_component = SEFIComponent()
 @sefi_component.model(name="InceptionV3(ImageNet)")
 def create_model(run_device):
     run_device = run_device if run_device is not None else ('cuda' if torch.cuda.is_available() else 'cpu')
-    norm_layer = Normalize(mean=[0.485, 0.456, 0.406], std=[0.229,0.224,0.225])
+    norm_layer = Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     inception_model = nn.Sequential(
         norm_layer,
         models.inception_v3(weights=Inception_V3_Weights.IMAGENET1K_V1)).to(run_device).eval()
