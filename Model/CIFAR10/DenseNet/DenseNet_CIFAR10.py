23d0ead6ad466b528611f88ee615fcbf7890fdc7
BearFromeAncient <wqxiong1211@163.com>
2023-03-03 16:32:18 +0800

    增加to(device)

--- Model/CIFAR10/DenseNet/DenseNet_CIFAR10.py
+++ Model/CIFAR10/DenseNet/DenseNet_CIFAR10.py
@@ -13,7 +13,7 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 def create_model(is_pretrained=True, no_normalize_layer=False):
     norm_layer = Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2471, 0.2435, 0.2616])
     if no_normalize_layer:
-        densenet_model = densenet161(pretrained=is_pretrained)
+        densenet_model = densenet161(pretrained=is_pretrained).to(device)
     else:
         densenet_model = nn.Sequential(
             norm_layer,
