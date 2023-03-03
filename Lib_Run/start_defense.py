07f4f01f51957f35bedc210426f864e0b445abcb
BearFromeAncient <wqxiong1211@163.com>
2023-03-03 16:32:49 +0800

    defense训练起始文件

--- Lib_Run/start_defense.py
+++ Lib_Run/start_defense.py
@@ -1,8 +1,6 @@
 from CANARY_SEFI.core.component.component_manager import SEFI_component_manager
 from CANARY_SEFI.core.function.basic.train_function import adv_defense_4_img_batch
 from CANARY_SEFI.entity.dataset_info_entity import DatasetInfo
-from CANARY_SEFI.service.security_evaluation import SecurityEvaluation
-from CANARY_SEFI.task_manager import task_manager
 from component_manager import init_component_manager
 from Defense_Method.Adversarial_Training.trades import sefi_component as trades_component
 
@@ -10,5 +8,6 @@ if __name__ == "__main__":
     init_component_manager()
     SEFI_component_manager.add(trades_component)
 
-    adv_defense_4_img_batch(defense_name="trades", defense_args={}, model_name="", model_args={}, img_proc_args={},
-                            dataset_info=DatasetInfo(dataset_name="CIFAR-10", dataset_seed=40376958655838027))
+    adv_defense_4_img_batch(defense_name="trades", defense_args={}, model_name="DenseNet(CIFAR-10)",
+                            model_args={"is_pretrained":False, "no_normalize_layer":True}, img_proc_args={},
+                            dataset_info=DatasetInfo(dataset_name="CIFAR-10", dataset_seed=40376958655838027, dataset_size=100))
