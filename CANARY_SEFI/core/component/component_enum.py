720959eaf319d8f760e697e9571febe9b6348699
BearFromeAncient <wqxiong1211@163.com>
2023-03-03 16:31:37 +0800

    增加defense

--- CANARY_SEFI/core/component/component_enum.py
+++ CANARY_SEFI/core/component/component_enum.py
@@ -4,7 +4,9 @@ from enum import Enum
 class ComponentType(Enum):
     ATTACK = "attack"
     MODEL = "model"
+    DEFENSE = "defense"
 
 
 class ComponentConfigHandlerType(Enum):
     ATTACK_PARAMS = "attack_params"
+    DEFENSE_PARAMS = "defense_params"
