5d4de5596e269ab9737be243fea925ac9dd76993
BearFromeAncient <wqxiong1211@163.com>
2023-03-07 10:14:04 +0800

    修改传入dataset

--- Defense_Method/Adversarial_Training/trades.py
+++ Defense_Method/Adversarial_Training/trades.py
@@ -1,13 +1,11 @@
-import os
-import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 import torch.optim as optim
+
 from CANARY_SEFI.core.component.component_decorator import SEFIComponent
 from CANARY_SEFI.core.component.component_enum import ComponentType, ComponentConfigHandlerType
-from CANARY_SEFI.handler.tools.foolbox_adapter import FoolboxAdapter
 
 sefi_component = SEFIComponent()
 
@@ -20,14 +18,14 @@ sefi_component = SEFIComponent()
                                       })
 
 class Trades():
-    def __init__(self, lr=0.1, momentum=0.9, weight_decay=2e-4, epochs=10, device="cuda", step_size = 0.007,
+    def __init__(self, lr=0.1, momentum=0.9, weight_decay=2e-4, epochs=10, run_device=None, step_size = 0.007,
                 epsilon = 0.031, num_steps = 10, beta = 6.0, log_interval = 5):
         #defense_args_dict传到这里初始化
         self.lr = lr
         self.momentum = momentum
         self.weight_decay = weight_decay
         self.epochs = epochs
-        self.device = device
+        self.device = run_device
         self.step_size = step_size
         self.epsilon = epsilon
         self.num_steps = num_steps
@@ -35,22 +33,19 @@ class Trades():
         self.log_interval = log_interval
 
     @sefi_component.defense(name="trades", is_inclass=True, support_model=[])
-    def defense(self, defense_model, imgs, labels):
+    def defense(self, defense_model, dataset, each_epoch_finish_callback=None):
         defense_model.train()
         optimizer = optim.SGD(defense_model.parameters(), lr=self.lr, momentum=self.momentum, weight_decay=self.weight_decay)
         # for epoch in range(1, self.epochs + 1):
-        for epoch in range(1):
+        for epoch in range(1, self.epochs + 1):
             # adjust learning rate for SGD
             self.adjust_learning_rate(optimizer,epoch)
 
             # adversarial training
-            for index in range(len(imgs)):
-                #print(np.array(data).shape)
-                #data = img_preprocessor(data, img_proc_args_dict)
-                data, target = imgs[index].to(self.device), labels[index].to(self.device)
-
+            for cur in dataset:
+                # data, target = imgs[index].to(self.device), labels[index].to(self.device)
+                data, target = cur[0].to(self.device), cur[1].to(self.device)
                 optimizer.zero_grad()
-                print("x_natural_{}:{}".format(index,data.tolist()))
                 # calculate robust loss
                 loss = self.trades_loss(model=defense_model,
                                         x_natural=data,
@@ -63,23 +58,19 @@ class Trades():
                 loss.backward()
                 optimizer.step()
 
-                # if index==10:
-                #     with open("x_natural_10.txt",'w') as output:
-                #         output.write(str(data.tolist()))
-                #         output.close()
-
                 # print progress
-            # print("epoch:{},loss:{:.6f}".format(epoch,loss.item()))
-                if index % self.log_interval == 0:
-                    print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                        epoch, index * len(data), len(imgs),
-                               100. * index / len(imgs), loss.item()))
+                # if index % self.log_interval == 0:
+                #     print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                #         epoch, index * len(data), len(imgs),
+                #                100. * index / len(imgs), loss.item()))
+
+            # print(epoch, each_epoch_finish_callback)
+            each_epoch_finish_callback(epoch)
 
         return defense_model.state_dict()
 
     def trades_loss(self, model, x_natural, y, optimizer, step_size=0.003, epsilon=0.031, perturb_steps=10, beta=1.0, distance='l_inf'):
         # define KL-loss
-
         criterion_kl = nn.KLDivLoss(size_average=False)
         model.eval()
         batch_size = len(x_natural)
@@ -108,7 +99,6 @@ class Trades():
         loss_robust = (1.0 / batch_size) * criterion_kl(F.log_softmax(model(x_adv), dim=1),
                                                         F.softmax(model(x_natural), dim=1))
         loss = loss_natural + beta * loss_robust
-        print("输出:",logits)
         return loss
 
     def adjust_learning_rate(self,optimizer, epoch):
