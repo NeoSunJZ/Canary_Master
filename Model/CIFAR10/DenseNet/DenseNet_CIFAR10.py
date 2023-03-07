1b3c269247d4949348b1a3d1bb585a5db3330fca
BearFromeAncient <wqxiong1211@163.com>
2023-03-07 10:12:43 +0800

    增加pretrained_file参数

--- Model/CIFAR10/DenseNet/DenseNet_CIFAR10.py
+++ Model/CIFAR10/DenseNet/DenseNet_CIFAR10.py
@@ -6,17 +6,16 @@ from CANARY_SEFI.core.component.component_decorator import SEFIComponent
 from Model.CIFAR10.DenseNet.densenet import densenet161
 
 sefi_component = SEFIComponent()
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
 @sefi_component.model(name="DenseNet(CIFAR-10)")
-def create_model(is_pretrained=True, pretrained_file=None, no_normalize_layer=False):
+def create_model(is_pretrained=True, pretrained_file=None, no_normalize_layer=False, run_device=None):
     norm_layer = Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2471, 0.2435, 0.2616])
     if no_normalize_layer:
-        densenet_model = densenet161(pretrained=is_pretrained, pretrained_file=pretrained_file).to(device)
+        densenet_model = densenet161(pretrained=is_pretrained, pretrained_file=pretrained_file).to(run_device)
     else:
         densenet_model = nn.Sequential(
             norm_layer,
             densenet161(pretrained=is_pretrained, pretrained_file=pretrained_file)
-        ).to(device).eval()
+        ).to(run_device).eval()
     return densenet_model
