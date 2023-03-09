14696c5832816ade78f1aa8a0f2dccbb06c93896
BearFromeAncient <wqxiong1211@163.com>
2023-03-09 15:24:36 +0800

    修改网络

--- Model/CIFAR10/Inception/InceptionV3_CIFAR10.py
+++ Model/CIFAR10/Inception/InceptionV3_CIFAR10.py
@@ -10,10 +10,13 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
 @sefi_component.model(name="InceptionV3(CIFAR-10)")
-def create_model():
+def create_model(is_pretrained=True, pretrained_file=None, no_normalize_layer=False, run_device=None):
     norm_layer = Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2471, 0.2435, 0.2616])
-    inception_model = nn.Sequential(
-        norm_layer,
-        inception_v3(pretrained=True)
-    ).to(device).eval()
-    return inception_model.eval()
+    if no_normalize_layer:
+        inception_model = inception_v3(pretrained=is_pretrained, pretrained_file=pretrained_file).to(run_device)
+    else:
+        inception_model = nn.Sequential(
+            norm_layer,
+            inception_v3(pretrained=is_pretrained, pretrained_file=pretrained_file)
+        ).to(device).eval()
+    return inception_model
