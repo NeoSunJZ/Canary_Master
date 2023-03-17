75960d9891fac4e4d4b651af0dccebf5a059e4ad
whoamTV <1252715200@qq.com>
2023-03-17 18:42:34 +0800

    增加防御-图片预处理方法类型

--- CANARY_SEFI/core/component/component_enum.py
+++ CANARY_SEFI/core/component/component_enum.py
@@ -5,8 +5,10 @@ class ComponentType(Enum):
     ATTACK = "attack"
     MODEL = "model"
     DEFENSE = "defense"
+    TRANS = "trans"
 
 
 class ComponentConfigHandlerType(Enum):
     ATTACK_PARAMS = "attack_params"
     DEFENSE_PARAMS = "defense_params"
+    TRANS_PARAMS = "trans_params"
