f23837caedcdf20618b9268f83623f236033f645
whoamTV <1252715200@qq.com>
2023-04-01 19:04:26 +0800

    优化了防御样本数据库查询策略，解决了重复执行防御样本生成会报错的问题

--- CANARY_SEFI/evaluator/logger/trans_file_info_handler.py
+++ CANARY_SEFI/evaluator/logger/trans_file_info_handler.py
@@ -9,14 +9,15 @@ def add_adv_trans_img_file_log(trans_name, attack_id, adv_img_file_id, adv_trans
           "VALUES (NULL,?,?,?,?,?)"
 
     sql_query = " SELECT adv_trans_img_file_id FROM adv_trans_img_file_log " \
-                " WHERE trans_name = ? AND attack_id = ? AND adv_img_file_id = ? AND adv_trans_img_filename = ? AND ground_valid = ?"
-    args = (trans_name, attack_id, adv_img_file_id, adv_trans_img_filename, ground_valid)
-    result = task_manager.test_data_logger.query_log(sql_query, args)
+                " WHERE trans_name = ? AND attack_id = ? AND adv_img_file_id = ? AND adv_trans_img_filename = ?"
+    args_insert = (trans_name, attack_id, adv_img_file_id, adv_trans_img_filename, ground_valid)
+    args_query = (trans_name, attack_id, adv_img_file_id, adv_trans_img_filename)
+    result = task_manager.test_data_logger.query_log(sql_query, args_query)
 
     if result is not None:
         adv_trans_img_file_id = result["adv_trans_img_file_id"]
     else:
-        adv_trans_img_file_id = task_manager.test_data_logger.insert_log(sql_insert, args)
+        adv_trans_img_file_id = task_manager.test_data_logger.insert_log(sql_insert, args_insert)
     if task_manager.test_data_logger.debug_log:
         msg = "[ LOGGER ] Logged. Adv_Trans file ID: {}. Trans name: {}. Trans Img file name: {}. Adv_Img file ID: {}."\
             .format(adv_trans_img_file_id, trans_name, adv_trans_img_filename, adv_img_file_id)
