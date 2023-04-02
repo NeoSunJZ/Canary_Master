6d47cfd74118d2ae87965413d013c314bb55d13a
whoamTV <1252715200@qq.com>
2023-04-02 23:57:16 +0800

    删除了防御样本迁移测试逻辑

--- CANARY_SEFI/core/function/test_and_evaluation.py
+++ CANARY_SEFI/core/function/test_and_evaluation.py
@@ -290,12 +290,13 @@ def trans_deflection_capability_test(attacker_list, model_config, img_proc_confi
             for trans_name in attacker_list[atk_name][base_model]:
 
                 test_on_model_list = []
-                if transfer_attack_test == TransferAttackType.NOT:
-                    test_on_model_list.append(base_model)
-                elif transfer_attack_test == TransferAttackType.APPOINT:
-                    test_on_model_list = transfer_attack_test_on_model_list[atk_name][base_model]
-                elif transfer_attack_test == TransferAttackType.SELF_CROSS:
-                    test_on_model_list = attacker_list[atk_name]
+                test_on_model_list.append(base_model)
+                # if transfer_attack_test == TransferAttackType.NOT:
+                #     test_on_model_list.append(base_model)
+                # elif transfer_attack_test == TransferAttackType.APPOINT:
+                #     test_on_model_list = transfer_attack_test_on_model_list[atk_name][base_model].copy()
+                # elif transfer_attack_test == TransferAttackType.SELF_CROSS:
+                #     test_on_model_list = attacker_list[atk_name].copy()
 
                 def function(model_name, model_args, img_proc_args, run_device):
                     # 攻击测试
