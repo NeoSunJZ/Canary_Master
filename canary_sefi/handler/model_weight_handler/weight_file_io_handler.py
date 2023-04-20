e23776f5049b803057ec13a2ab27e9e3f8ad10b1
BearFromeAncient <wqxiong1211@163.com>
2023-04-20 21:06:53 +0800

    在保存权重文件同时将文件信息记入数据库

--- canary_sefi/handler/model_weight_handler/weight_file_io_handler.py
+++ canary_sefi/handler/model_weight_handler/weight_file_io_handler.py
@@ -1,9 +1,11 @@
 import os
 import torch
+
+from canary_sefi.evaluator.logger.adv_training_weight_path_info_handler import add_adv_training_weight_file_path_log
 from canary_sefi.task_manager import task_manager
 
 
-def save_weight_to_temp(file_path, file_name, weight):
+def save_weight_to_temp(model_name, defense_name, epoch_cursor, file_path, file_name, weight):
     full_path = task_manager.base_temp_path + "weight/" + file_path
     if not os.path.exists(full_path):
         os.makedirs(full_path)
@@ -11,4 +13,5 @@ def save_weight_to_temp(file_path, file_name, weight):
     full_path = full_path + file_name
     # Save model
     torch.save(weight, full_path)
+    add_adv_training_weight_file_path_log(model_name, defense_name, epoch_cursor, full_path)
     return
