5592cfa6f8237d6270cc52b27c3d3e1506ae9930
BearFromeAncient <wqxiong1211@163.com>
2023-04-20 20:54:36 +0800

    批量导入对抗训练组件

--- canary_lib/__init__.py
+++ canary_lib/__init__.py
@@ -1,6 +1,7 @@
 from .canary_attack_method import attacker_list
 from .canary_model import model_list
+from .canary_defense_method import defender_list
 from .copyright import print_logo
 
 print_logo()
-canary_lib = attacker_list + model_list
+canary_lib = attacker_list + model_list + defender_list
---
+++ canary_lib/canary_defense_method/__init__.py
@@ -0,0 +1,8 @@
+from .adversarial_training import *
+
+defender_list = [
+    nat_defender,
+    mart_defender,
+    trades_defender,
+    natural_defender,
+]
