e9be27f8b913cfc33c6fe44850afc83e2e4a0881
NeoSunJZ <neosunjz@qq.com>
2023-09-26 15:21:11 +0800

    修正了数据集DATASET_LOADER在自动增加默认加载器时存在的问题

--- canary_sefi/core/component/default_component/dataset_getter.py
+++ canary_sefi/core/component/default_component/dataset_getter.py
@@ -8,7 +8,9 @@ from colorama import Fore
 from numpy.compat import long
 from torch.utils.data import DataLoader
 from torchvision.datasets import ImageFolder
-from canary_sefi.core.component.component_enum import SubComponentType
+
+from canary_sefi.core.component.component_dict import ComponentDict, ComponentDictType
+from canary_sefi.core.component.component_enum import SubComponentType, ComponentType
 from canary_sefi.core.component.component_manager import SEFI_component_manager
 from canary_sefi.core.function.helper.realtime_reporter import reporter
 from canary_sefi.entity.dataset_info_entity import DatasetType
@@ -27,18 +29,22 @@ def get_dataset(dataset_info):
         reporter.console_log(msg, Fore.BLUE, show_task=True, show_step_sequence=True)
         dataset = default_folder_dataset_getter(copy.copy(dataset_info))
     if dataset is not None:
-        SEFI_component_manager.dataset_list[dataset_info.dataset_name] = {
+        SEFI_component_manager.dataset_list[dataset_info.dataset_name] = ComponentDict({
             SubComponentType.DATASET_LOADER: default_folder_dataset_getter
-        }
+        },
+            dict_type=ComponentDictType.ComponentDict,
+            component_type=ComponentType.DATASET)
     else:
         msg = "[SEFI] User has NOT defined dataset loader. Attempting to load dataset by TorchVision's Dataset..."
         reporter.console_log(msg, Fore.BLUE, show_task=True, show_step_sequence=True)
         dataset = default_torchvision_dataset_getter(copy.copy(dataset_info))
         # 检查是否仍然为空
         if dataset is not None:
-            SEFI_component_manager.dataset_list[dataset_info.dataset_name] = {
+            SEFI_component_manager.dataset_list[dataset_info.dataset_name] = ComponentDict({
                 SubComponentType.DATASET_LOADER: default_torchvision_dataset_getter
-            }
+            },
+                dict_type=ComponentDictType.ComponentDict,
+                component_type=ComponentType.DATASET)
         else:
             raise Exception("[SEFI] After exhausting possible loading methods, it has not been successful. "
                             "Please configure the loader or check if the configuration is correct")
@@ -64,12 +70,13 @@ def default_folder_dataset_getter(dataset_info):
         def __getitem__(self, index: int) -> Tuple[Any, Any]:
             sample, target = super().__getitem__(index)
             return np.array(sample, dtype=np.uint8), target
+
     try:
         dataset = ImageFolderCustom(root=dataset_info.dataset_path)
         return dataset
     except Exception as e:
-        msg = "[SEFI] INFO: User has NOT defined dataset loader "\
-              "AND default dataset loader is used BUT FAILED TO IMPORT DATASET FROM PATH\n"\
+        msg = "[SEFI] INFO: User has NOT defined dataset loader " \
+              "AND default dataset loader is used BUT FAILED TO IMPORT DATASET FROM PATH\n" \
               "Info: {}".format(e)
         reporter.console_log(msg, Fore.RED, show_task=True, show_step_sequence=True)
         return None
@@ -83,8 +90,8 @@ def default_torchvision_dataset_getter(dataset_info):
         dataset = dataset_class(root=dataset_info.dataset_path, train=is_train, download=True)
         return dataset
     except Exception as e:
-        msg = "[SEFI] INFO: User has NOT defined dataset loader "\
-              "AND default dataset loader is used BUT FAILED TO IMPORT DATASET BY TORCHVISION DATASET\n"\
+        msg = "[SEFI] INFO: User has NOT defined dataset loader " \
+              "AND default dataset loader is used BUT FAILED TO IMPORT DATASET BY TORCHVISION DATASET\n" \
               "Info: {}".format(e)
         reporter.console_log(msg, Fore.RED, show_task=True, show_step_sequence=True)
         return None
